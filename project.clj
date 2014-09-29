fe9dbc96e6c0d20ff08ce6f6275ba3ca89607747
Jude Payne <judepayne@yahoo.co.uk>
2014-09-29 23:14:11 +0100

    secured open data

--- project.clj
+++ project.clj
@@ -7,6 +7,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [incanter "1.5.5"]
-                 [org.jfree/jfreechart "1.0.19"]]
-  :signing {:gpg-key "5C92FAF1"}
-  :deploy-repositories [["clojars" {:creds :gpg}]])
+                 [org.jfree/jfreechart "1.0.19"]])
